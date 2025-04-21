9927bdc06872c0021e2774abc5342cbe9582cbec
Mbalis-dot <Mskhosana19@gmail.com>
2025-04-21 18:20:28 +0000

    0x00-ES6_basic

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+  
+
+  return [task, task2];
+}
